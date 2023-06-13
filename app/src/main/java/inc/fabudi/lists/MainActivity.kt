6de63d5a87727d66d2c43ede0e73be08fc5b3b94
Fabudi <evgeniydodg@gmail.com>
2023-06-13 10:42:59 +0300

    Basic activity with 5 Fragments and navigation via BottomNavigationBar

---
+++ app/src/main/java/inc/fabudi/lists/MainActivity.kt
@@ -0,0 +1,32 @@
+package inc.fabudi.lists
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val navigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.firstFragment,
+                R.id.secondFragment,
+                R.id.thirdFragment,
+                R.id.fourthFragment,
+                R.id.fifthFragment
+            )
+        )
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment_container) as NavHostFragment
+        val navController = navHostFragment.navController
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navigationView.setupWithNavController(navController)
+    }
+
+}
---
+++ app/src/main/java/inc/fabudi/lists/ThirdFragment.kt
@@ -0,0 +1,10 @@
+package inc.fabudi.lists
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+
+class ThirdFragment: Fragment(R.layout.third_layout) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
